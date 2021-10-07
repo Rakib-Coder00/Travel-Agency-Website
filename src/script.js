360a679cdba62cbfccb225aaa365d94bde360542
Limitless-Rakib <80609225+Rakib-Coder00@users.noreply.github.com>
2021-10-07 20:52:15 +0600

    home section added

--- src/script.js
+++ src/script.js
@@ -38,4 +38,16 @@ menu.addEventListener('click', ()=>{
     navBar.classList.toggle('active')
 })
 
+// videoButton
+let videoBtn = document.querySelectorAll('.vid-btn')
+
+videoBtn.forEach(btn =>{
+    btn.addEventListener('click',()=>{
+        document.querySelector('.controls .active').classList.remove('active')
+        btn.classList.add('active')
+
+        let src = btn.getAttribute('data-src')
+        document.querySelector('#video-slider').src = src
+    })
+})
 
