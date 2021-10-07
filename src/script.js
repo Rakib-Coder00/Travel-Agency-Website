c2bddd8b22597fb5b2a935930c895b62e739d019
Limitless-Rakib <80609225+Rakib-Coder00@users.noreply.github.com>
2021-10-07 11:10:28 +0600

    login form added

--- src/script.js
+++ src/script.js
@@ -1,13 +1,41 @@
+// searchButton
 let searchBtn = document.querySelector('#search-btn')
 let searchBar = document.querySelector('.search-bar-container')
-
 
 searchBtn.addEventListener('click', ()=>{
     searchBtn.classList.toggle('fa-times')
     searchBar.classList.toggle('active')
 })
-
+// onScroll remove class 
 window.onscroll = ()=>{
     searchBtn.classList.remove('fa-times')
     searchBar.classList.remove('active')
+
+    menu.classList.remove('fa-times')
+    navBar.classList.remove('active')
+    
+    loginForm.classList.remove('active')
 }
+
+// loginForm 
+let formBtn = document.querySelector('#login-btn')
+let loginForm = document.querySelector('.login-form-container')
+let formClose = document.querySelector('#form-close')
+
+formBtn.addEventListener('click',()=>{
+    loginForm.classList.add('active')
+})
+formClose.addEventListener('click',()=>{
+    loginForm.classList.remove('active')
+})
+
+// menuBar 
+let menu = document.querySelector('#menu-bar')
+let navBar = document.querySelector('.navbar')
+
+menu.addEventListener('click', ()=>{
+    menu.classList.toggle('fa-times')
+    navBar.classList.toggle('active')
+})
+
+
